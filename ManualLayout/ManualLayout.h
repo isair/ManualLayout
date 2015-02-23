eb06fba9408f523d22267758095f0a21961721b1
Baris Sencan <barissncn@gmail.com>
2015-02-23 12:03:57 -0800

    Initial commit

---
+++ ManualLayout/ManualLayout.h
@@ -0,0 +1,19 @@
+//
+//  ManualLayout.h
+//  ManualLayout
+//
+//  Created by Baris Sencan on 23/02/15.
+//  Copyright (c) 2015 Baris Sencan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ManualLayout.
+FOUNDATION_EXPORT double ManualLayoutVersionNumber;
+
+//! Project version string for ManualLayout.
+FOUNDATION_EXPORT const unsigned char ManualLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ManualLayout/PublicHeader.h>
+
+
