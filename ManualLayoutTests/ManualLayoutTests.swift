8d98f1927eb77bfbc57039232cd5dd035766ca85
Baris Sencan <barissncn@gmail.com>
2015-02-25 15:44:24 -0800

    Test fast access properties

--- ManualLayoutTests/ManualLayoutTests.swift
+++ ManualLayoutTests/ManualLayoutTests.swift
@@ -15,14 +15,14 @@ internal class ManualLayoutTests: XCTestCase {
   let rect = CGRect(x: 0, y: 0, width: 10, height: 10)
 
   func testGetOriginForCenteringRect() {
-    let origin = ManualLayout.getOriginForCenteringRect(rect, inRect: otherRect)
-    XCTAssertEqual(origin, CGPoint(x: 55, y: 65), "origin should be at (55, 65)")
+    let o = ManualLayout.getOriginForCenteringRect(rect, inRect: otherRect)
+    XCTAssertEqual(o, CGPoint(x: 55, y: 65), "o should equal center point")
   }
 
   func testGetPositionForCenteringRect() {
-    let positionX = ManualLayout.getPositionForCenteringRect(rect, dimension: .X, inRect: otherRect)
-    XCTAssert(positionX == 55, "x position should be 55")
-    let positionY = ManualLayout.getPositionForCenteringRect(rect, dimension: .Y, inRect: otherRect)
-    XCTAssert(positionY == 65, "y position should be 65")
+    let x = ManualLayout.getPositionForCenteringRect(rect, dimension: .X, inRect: otherRect)
+    XCTAssert(x == 55, "x should be in center")
+    let y = ManualLayout.getPositionForCenteringRect(rect, dimension: .Y, inRect: otherRect)
+    XCTAssert(y == 65, "y should be in center")
   }
 }
