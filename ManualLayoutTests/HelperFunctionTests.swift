50d2f2e16a5ef0f569dea8fbbc1b536b369d366b
Baris Sencan <barissncn@gmail.com>
2015-02-26 15:56:42 -0800

    Test helper functions

--- ManualLayoutTests/HelperFunctionTests.swift
+++ ManualLayoutTests/HelperFunctionTests.swift
@@ -18,11 +18,43 @@ class HelperFunctionTests: XCTestCase {
     view.frame = defaultFrame
   }
 
-  func testInset() {
-    
+  func testInsetSingleArg() {
+    view.frame = inset(view, 1)
+    XCTAssertEqual(
+      view.frame,
+      CGRect(x: 2, y: 4, width: 4, height: 6),
+      "insetting with amount should inset correctly")
   }
 
-  func testOffset() {
+  func testInsetTwoArg() {
+    view.frame = inset(view, 1, 2)
+    XCTAssertEqual(
+      view.frame,
+      CGRect(x: 2, y: 5, width: 4, height: 4),
+      "insetting with dx and dy should inset correctly")
+  }
 
+  func testInsetFourArg() {
+    view.frame = inset(view, 1, 2, 3, 4)
+    XCTAssertEqual(
+      view.frame,
+      CGRect(x: 3, y: 4, width: 0, height: 4),
+      "insetting with four arguments should inset correctly")
+  }
+
+  func testOffsetSingleArg() {
+    view.frame = offset(view, 1)
+    XCTAssertEqual(
+      view.frame,
+      CGRect(x: 2, y: 4, width: 6, height: 8),
+      "offsetting with amount should offset correctly")
+  }
+
+  func testOffsetTwoArg() {
+    view.frame = offset(view, 1, 2)
+    XCTAssertEqual(
+      view.frame,
+      CGRect(x: 2, y: 5, width: 6, height: 8),
+      "offsetting with dx and dy should offset correctly")
   }
 }
