d5f0c1f666955fdf8f7673dc9b9446230b3290ab
Baris Sencan <barissncn@gmail.com>
2015-02-26 15:40:07 -0800

    Add inset and offset functions

---
+++ ManualLayoutTests/HelperFunctionTests.swift
@@ -0,0 +1,28 @@
+//
+//  HelperFunctionTests.swift
+//  ManualLayout
+//
+//  Created by Baris Sencan on 26/02/15.
+//  Copyright (c) 2015 Baris Sencan. All rights reserved.
+//
+
+import Foundation
+import XCTest
+import ManualLayout
+
+class HelperFunctionTests: XCTestCase {
+  var view = UIView(frame: CGRectZero)
+  let defaultFrame = CGRect(x: 1, y: 3, width: 6, height: 8)
+
+  override func setUp() {
+    view.frame = defaultFrame
+  }
+
+  func testInset() {
+    
+  }
+
+  func testOffset() {
+
+  }
+}
